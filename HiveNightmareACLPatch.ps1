eda3f98ff1b5399ca7aba90320529cf5b9912cca
FFFreak <38145587+FFFreak@users.noreply.github.com>
2021-07-20 22:28:42 -0700

    Updated function for Positive and Negative Cases configured with a boolean switch

--- HiveNightmareACLPatch.ps1
+++ HiveNightmareACLPatch.ps1
@@ -10,10 +10,18 @@ function CheckResult
     [Parameter(Mandatory=$true, Position=0)]
     [string] $strCommandOutput,
     [Parameter(Mandatory=$true, Position=1)]
-    [string] $strIntendedResult
+    [string] $strIntendedResult,
+    [Parameter(Mandatory=$true, Position=2)]
+    [Switch] $blnPresent
   )
+  
+  # Convert for Select-String 
+  $strIntendedResult = [regex]::Escape($strIntendedResult)
 
-  if (($strCommandOutput | Select-String $strIntendedResult).length -gt 0) {
+  if (
+      (($strCommandOutput | Select-String "$strIntendedResult").length -gt 0 -and $blnPresent) -or
+      (($strCommandOutput | Select-String "$strIntendedResult").length -eq 0 -and -not $blnPresent)
+     ) {
     Write-host ".. Success" -Foreground Green
     return $true
   } else {
@@ -28,33 +36,53 @@ $intTestsRan = 0
 
 # Successful String for icals commands
 $strResultSuccessful = "Successfully processed 1 files; Failed processing 0 files"
+$blnSuccesIfPresent = $true
+
+$strResultUnSuccessful = "BUILTIN\Users:(I)(RX)"
+$blnFailIfPresent = $false
 
 # SAM File
-$intTestsRan++
 Write-host "Setting SAM file"
+Write-host "`tRemoving ACL"
 $strCommand = icacls "$env:windir\system32\config\sam" /remove "Users"
-if (CheckResult [String]$strCommand $strResultSuccessful) {$intTestsSuccessful++}
+$intTestsRan++
+if (CheckResult [String]$strCommand $strResultSuccessful $blnSuccesIfPresent) {$intTestsSuccessful++}
+$intTestsRan++
+Write-host "`tValidating ACL"
+$strCommand = icacls "$env:windir\system32\config\sam"
+if (CheckResult [String]$strCommand $strResultUnSuccessful $blnFailIfPresent) {$intTestsSuccessful++}
 
 # Security File
-$intTestsRan++
 Write-host "Setting Security file"
+Write-host "`tRemoving ACL"
 $strCommand = icacls "$env:windir\system32\config\security" /remove "Users"
-if (CheckResult [String]$strCommand $strResultSuccessful) {$intTestsSuccessful++}
+$intTestsRan++
+if (CheckResult [String]$strCommand $strResultSuccessful $blnSuccesIfPresent) {$intTestsSuccessful++}
+$intTestsRan++
+Write-host "`tValidating ACL"
+$strCommand = icacls "$env:windir\system32\config\security"
+if (CheckResult [String]$strCommand $strResultUnSuccessful $blnFailIfPresent) {$intTestsSuccessful++}
 
 # System File
-$intTestsRan++
 Write-host "Setting System file"
-$strCommand = icacls "$env:windir\system32\config\security" /remove "Users"
-if (CheckResult [String]$strCommand $strResultSuccessful) {$intTestsSuccessful++}
+Write-host "`tRemoving ACL"
+$strCommand = icacls "$env:windir\system32\config\system" /remove "Users"
+$intTestsRan++
+if (CheckResult [String]$strCommand $strResultSuccessful $blnSuccesIfPresent) {$intTestsSuccessful++}
+$intTestsRan++
+Write-host "`tValidating ACL"
+$strCommand = icacls "$env:windir\system32\config\system"
+if (CheckResult [String]$strCommand $strResultUnSuccessful $blnFailIfPresent) {$intTestsSuccessful++}
 
 # Clear VSS
-$intTestsRan++
 $strResultSuccessful = "No items found that satisfy the query."
 Write-host "Clearing VSS"
 vssadmin delete shadows /for=c: /Quiet | out-null
+
 Write-host "Validating cleared VSS"
+$intTestsRan++
 $strCommand = vssadmin list shadows
-if (CheckResult [String]$strCommand $strResultSuccessful) {$intTestsSuccessful++}
+if (CheckResult [String]$strCommand $strResultSuccessful $blnSuccesIfPresent) {$intTestsSuccessful++}
 
 if ($intTestsRan -eq $intTestsSuccessful) {
   write-host "`r`n`t ** All Commands Successful" -Foreground Green
